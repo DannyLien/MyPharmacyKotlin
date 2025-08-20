8f9d0abae64bd9eb50ab694907fc26421f9b74d2
DannyLien <danny.lien1109@gmail.com>
2025-08-20 17:05:54 +0800

    set, Pharmacy Data Load, spinner: City & Town;

--- app/src/main/java/com/hom/pharmacy/MainActivity.kt
+++ app/src/main/java/com/hom/pharmacy/MainActivity.kt
@@ -3,15 +3,27 @@ package com.hom.pharmacy
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemSelectedListener
+import android.widget.ArrayAdapter
+import android.widget.ProgressBar
+import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
 import com.hom.pharmacy.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var progBar: ProgressBar
+    private lateinit var vm: PharmacyViewModel
+    private lateinit var recy: RecyclerView
+    private lateinit var spTown: Spinner
+    private lateinit var spCity: Spinner
     private var pharmInfo: PharmacyInfo? = null
     private val TAG: String? = MainActivity::class.java.simpleName
     private lateinit var binding: ActivityMainBinding
@@ -26,14 +38,74 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        Log.d(TAG, "onCreate: mask-")
-        val vm = ViewModelProvider(this).get(PharmacyViewModel::class.java)
+//        Log.d(TAG, "onCreate: mask-")
+        findViews()
+        vm = ViewModelProvider(this).get(PharmacyViewModel::class.java)
         vm.upDataPharmacy()
-        vm.getPharmInfo.observe(this){
+        progBar.visibility = View.VISIBLE
+        vm.getPharmInfo.observe(this) {
             pharmInfo = it
-            Log.d(TAG, "onCreate: mask-pharmInfo- ${it}")
+//            Log.d(TAG, "onCreate: mask-pharmInfo- ${it}")
+        }
+        vm.getAllCity.observe(this) {
+            setSPCity(it)
+//            Log.d(TAG, "onCreate: mask-getAllCity- ${it}")
+        }
+        vm.getAllTown.observe(this) {
+            progBar.visibility = View.GONE
+            setSPTown(it)
+//            Log.d(TAG, "onCreate: mask-getAllTown- ${it}")
         }
 
+    }
+
+    private fun findViews() {
+        progBar = binding.progBar
+        spCity = binding.spCity
+        spTown = binding.spTown
+        recy = binding.recyclerPharm
+    }
+
+    var currentCity = ""
+    private fun setSPCity(cityName: List<String>) {
+        val cityAdapter =
+            ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, cityName)
+                .apply { setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item) }
+        spCity.adapter = cityAdapter
+        spCity.prompt = " Select City "
+        spCity.onItemSelectedListener = object : OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?, view: View?, position: Int, id: Long
+            ) {
+                currentCity = spCity.selectedItem.toString()
+                vm.vmUpDataTown(currentCity)
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+        }
+    }
+
+    var currentTown = ""
+    private fun setSPTown(townName: List<String>) {
+        val townAdapter =
+            ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, townName)
+                .apply { setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item) }
+        spTown.adapter = townAdapter
+        spTown.prompt = " Select Town "
+        spTown.onItemSelectedListener = object : OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?, view: View?, position: Int, id: Long
+            ) {
+                currentTown = spTown.selectedItem.toString()
+//                setRecyPharm()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+        }
     }
 
 
--- app/src/main/java/com/hom/pharmacy/PharmacyViewModel.kt
+++ app/src/main/java/com/hom/pharmacy/PharmacyViewModel.kt
@@ -9,20 +9,55 @@ import kotlinx.coroutines.launch
 import java.net.URL
 
 class PharmacyViewModel : ViewModel() {
+
+    var allTown = mutableListOf<String>()
+    var getAllTown = MutableLiveData<List<String>>()
+    var allCity = mutableListOf<String>()
+    var getAllCity = MutableLiveData<List<String>>()
+    private var pharmInfo: PharmacyInfo? = null
+    var getPharmInfo = MutableLiveData<PharmacyInfo>()
+
     companion object {
         val pharmUrl_5K = "http://delexons.ddns.net:81/pharmacies/info.json"
         val pharmUrl_132 = "http://delexons.ddns.net:81/pharmacies/info_132.json"
     }
 
-    private var pharmInfo: PharmacyInfo? = null
-    var getPharmInfo = MutableLiveData<PharmacyInfo>()
+    object PharmInfoData {
+        var pharmInfoGson: PharmacyInfo? = null
+    }
+
     fun upDataPharmacy() {
         CoroutineScope(Dispatchers.IO).launch {
             val json = URL(pharmUrl_132).readText()
             pharmInfo = Gson().fromJson(json, PharmacyInfo::class.java)
             getPharmInfo.postValue(pharmInfo!!)
+            PharmInfoData.pharmInfoGson = pharmInfo
+            vmUpDataCity()
         }
     }
+
+    fun vmUpDataCity() {
+        pharmInfo.also { pharm ->
+            val data = pharm?.features?.groupBy { it.properties.county }
+            data?.forEach {
+                allCity.add(it.key)
+            }
+        }
+        getAllCity.postValue(allCity)
+    }
+
+    fun vmUpDataTown(currentCity: String) {
+        pharmInfo.also { pharm ->
+            val data = pharm?.features?.filter { it.properties.county == currentCity }
+            allTown.clear()
+            data?.forEach {
+                allTown.add(it.properties.town)
+            }
+            allTown = allTown.distinct().toMutableList()
+            getAllTown.postValue(allTown)
+        }
+    }
+
 
 }
 
